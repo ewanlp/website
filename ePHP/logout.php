ee272b01f08087639d795d56350d112523d93435
ewanlp <ewanlp@gmail.com>
2017-06-26 11:34:54 -0600

    Update logout.php

--- ePHP/logout.php
+++ ePHP/logout.php
@@ -3,6 +3,7 @@ session_start();
 require_once 'class.user.php';
 $user = new USER();
 
+//creates that user, and checks if they're not logged in, then if they are, they're signed out.
 if(!$user->is_logged_in())
 {
  $user->redirect('index.php');
